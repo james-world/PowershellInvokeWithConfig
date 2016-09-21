ac58ce42a7bad40582b0368716caa9d1032ea1a4
James World <james-world@users.noreply.github.com>
2016-09-21 17:01:30 +0100

    Add script demo

--- PowershellInvokeWithConfig/Demo.cs
+++ PowershellInvokeWithConfig/Demo.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PowershellInvokeWithConfig
 {
-    public class Class1
+    public class Demo
     {
+        public static void Foo()
+        {
+            Console.Write($"Foo read {ConfigurationManager.AppSettings["Test"]} from config file.");
+        }
     }
 }
---
+++ PowershellInvokeWithConfig/Script.ps1
@@ -0,0 +1,10 @@
+﻿function RunDemo {
+	Push-Location $PSScriptRoot
+		$path = "$PSScriptRoot\PowershellInvokeWithConfig.dll";
+		[System.AppDomain]::CurrentDomain.SetData("APP_CONFIG_FILE", "$($path).config")
+		[Reflection.Assembly]::LoadFrom($path)
+		$demo = [PowershellInvokeWithConfig.Demo]::Foo();
+	Pop-Location
+}
+
+RunDemo
