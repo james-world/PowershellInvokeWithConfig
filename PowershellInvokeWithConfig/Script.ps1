4676b469a23a56c0a498a8ef79e21df1cdcb55c3
James World <james-world@users.noreply.github.com>
2016-09-21 17:24:46 +0100

    Remove spurious output

--- PowershellInvokeWithConfig/Script.ps1
+++ PowershellInvokeWithConfig/Script.ps1
@@ -2,7 +2,7 @@
 	Push-Location $PSScriptRoot
 		$path = "$PSScriptRoot\PowershellInvokeWithConfig.dll";
 		[System.AppDomain]::CurrentDomain.SetData("APP_CONFIG_FILE", "$($path).config")
-		[Reflection.Assembly]::LoadFrom($path)
+		[void][Reflection.Assembly]::LoadFrom($path)
 		$demo = [PowershellInvokeWithConfig.Demo]::Foo();
 	Pop-Location
 }
